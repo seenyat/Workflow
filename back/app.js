a462be2825f41d23a04de381b12f1639a8d98732
Roman Zagrebin <seenyat@gmail.com>
2021-05-12 17:17:35 +0300

    initial commit

---
+++ back/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
