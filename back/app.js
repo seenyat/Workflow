e85c173ed2e6ec7d8b7ad8cea1aaa2143733d7c9
Andrey <trifonova86@mail.ru>
2021-05-12 19:09:05 +0300

    routes

--- back/app.js
+++ back/app.js
@@ -1,7 +1,11 @@
 import middleware from './middleware/index.js'
 import express from "express";
-
+import ansRouter from './routes/answers'
+import questRouter from './routes/questions' 
 const app = express();
 middleware(app);
+
+app.use('/',ansRouter)
+app.use('/',questRouter)
 
 export default app
---
+++ back/routes/answers.js
@@ -0,0 +1,8 @@
+import router from "express"
+const routerAnswer = router.Router()
+import Answer from '../models/Answer'
+
+
+
+
+export default routerAnswer
