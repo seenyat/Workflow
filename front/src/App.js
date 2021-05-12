056c8ee0e40ff73c47d01a8bb8227ef383bd7d1c
Anton <antonmanafov@Mail.ru>
2021-05-12 19:10:39 +0300

    form ready

--- front/src/App.js
+++ front/src/App.js
@@ -3,16 +3,19 @@ import { Provider } from "react-redux";
 import store from "./Redux/store";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navigation from "./Views/Navigation";
+import UserProfile from "./Views/UserProfile";
 
 function App() {
   return (
     <Provider store={store}>
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
       <BrowserRouter>
         <Navigation />
 
-        <Switch></Switch>
+        <Switch>
+          <Route path="/">
+            <UserProfile />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </Provider>
   );
--- front/src/Redux/store.js
+++ front/src/Redux/store.js
@@ -1,28 +1,23 @@
-  import {createStore, applyMiddleware} from 'redux'
-  import {composeWithDevTools} from 'redux-devtools-extension'
-  import reducer from './reducer'
-  import createSagaMiddleware from 'redux-saga'
-  import {all} from 'redux-saga/effects'
-  import Sagas from './sagas'
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducer from "./reducer";
+import createSagaMiddleware from "redux-saga";
+import { all } from "redux-saga/effects";
+import Sagas from "./sagas";
 
-  const preloadedState = window.localStorage.getItem('state') || '{"Auth": false, "User":{}, "loading": false, "Questions": [] }';
+const preloadedState =
+  window.localStorage.getItem("state") ||
+  '{"Auth": false, "User":{"Username": "Anton", "email": "anton@mail.ru", "role": "admin" }, "loading": false, "Questions": [] }';
 
-  const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
-  const store = createStore(reducer, JSON.parse(preloadedState), composeWithDevTools(
-    applyMiddleware(
-      sagaMiddleware 
-    )
-  ))
+const store = createStore(
+  reducer,
+  JSON.parse(preloadedState),
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 
-  sagaMiddleware.run(
-    function* () {
-      yield all (
-        [
-          Sagas()
-        ]
-      )
-    }
- )
-  export default store
-  
+sagaMiddleware.run(function* () {
+  yield all([Sagas()]);
+});
+export default store;
