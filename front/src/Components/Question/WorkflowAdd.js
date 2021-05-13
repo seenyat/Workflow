d32f9a87d1f080b18a27580d63d345400e6cb2a3
Roman Zagrebin <seenyat@gmail.com>
2021-05-13 19:40:07 +0300

    Todo Focus Management

--- front/src/Components/Question/WorkflowAdd.js
+++ front/src/Components/Question/WorkflowAdd.js
@@ -5,7 +5,15 @@ import ToDoGroup from "./ToDoGroup";
 export default function WorkflowAdd({ todo, setTodo }) {
   return (
     <div className="bg-gray-100 pt-5 px-3 container flex flex-col items-center justify-center shadow overflow-hidden sm:rounded-lg">
-      <ToDoGroup />
+      {todo.map((el) => (
+        <ToDoGroup
+          todoList={el.todos}
+          Header={el.title}
+          todo={todo}
+          setTodo={setTodo}
+        />
+      ))}
+
       <button
         type="button"
         className="mx-auto my-2 p-1 text-white flex border-2 border-gray-300 bg-indigo-600 items-center justify-center w-10 h-10 rounded-full"
