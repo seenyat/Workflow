2a4fc8eeae07ffb8457d01b8f7f44b4a2ede219f
Andrey <trifonova86@mail.ru>
2021-05-13 11:12:50 +0300

    answerFind

--- back/routes/answers.js
+++ back/routes/answers.js
@@ -1,13 +1,16 @@
 import express from "express";
 const router = express.Router();
 import Answer from "../models/Answer.js";
+import Question from "../models/Question.js";
 
 router.post("/", async (req, res) => {
-  const { workflows, comment } = req.body;
+  const { workflows, comment, id  } = req.body;
   const answer = await Answer.create({
     workflows,
     comment,
   });
+  const question = await Question.findById(id)
+  question={...question,answers:question.answers}
   res.json(answer);
 });
 
