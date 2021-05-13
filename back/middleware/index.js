9dd1a98bad6438aee136a29f3c0f9f5c4a73c606
Ilia <ershovilya97@gmail.com>
2021-05-13 09:37:42 +0300

    change routes

--- back/middleware/index.js
+++ back/middleware/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
+import questionRouter from "../routes/question.js";
+import allQuestionsRouter from "../routes/allQuestions.js";
+import answerRouter from "../routes/answers.js";
 
 const config = (app) => {
   app.use(express.json());
@@ -9,11 +12,14 @@ const config = (app) => {
   app.use(morgan("dev"));
   app.use(cors());
 
+  app.use("/question", questionRouter);
+  app.use("/allquestions", allQuestionsRouter);
+  app.use("/answer", answerRouter);
+
   mongoose.connect("mongodb://localhost:27017/Prep", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  
 };
 
 export default config;
