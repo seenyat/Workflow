080ffc884209234a27828bfc5b29fd612a798ca2
Ilia <ershovilya97@gmail.com>
2021-05-13 14:48:42 +0300

    upd passport

--- back/models/User.js
+++ back/models/User.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  avatar_url: String,
+  login: String,
+  email: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import passport from "passport";
 import GitHubStrategy from "passport-github";
 import dotenv from "dotenv";
+import User from "../models/User.js";
 dotenv.config();
 
 const github = GitHubStrategy.Strategy;
@@ -25,22 +26,16 @@ passport.use(
       callbackURL: "http://localhost:4000/login/auth/github/callback",
     },
     function (accessToken, refreshToken, profile, cb) {
-      // User.findOrCreate({ githubId: profile.id }, function (err, user) {
-      //   return cb(err, user);
-      // });
-      console.log(profile);
-      cb(null, profile);
+      return cb(null, profile);
     }
   )
 );
-
 
 router.get("/", async (req, res) => {
   res.send("main");
 });
 
 router.get("/login", async (req, res) => {
-  console.log(req.session.passport);
   res.send("login");
 });
 
@@ -49,15 +44,24 @@ router.get("/login/auth/github", passport.authenticate("github"));
 router.get(
   "/login/auth/github/callback",
   passport.authenticate("github", { failureRedirect: "/" }),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect("/login");
+  async function (req, res) {
+    const { name, avatar_url, login, email } = req.user._json;
+    let user = await User.findOne({ login: login });
+    if (!user) {
+      user = await User.create({
+        name,
+        avatar_url,
+        login,
+        email,
+      });
+    }
+    res.status(200).json(req.user._json);
   }
 );
 
-router.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.redirect('/')
-})
+router.get("/logout", (req, res) => {
+  res.clearCookie("Workflow");
+  res.redirect("/");
+});
 
 export default router;
