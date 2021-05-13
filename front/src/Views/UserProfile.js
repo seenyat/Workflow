f8ed7a26af8f594cf0d106f718ea29be2ebefa07
Andrey <trifonova86@mail.ru>
2021-05-13 16:53:47 +0300

    profile

--- front/src/Views/UserProfile.js
+++ front/src/Views/UserProfile.js
@@ -1,6 +1,257 @@
-// import React from "react";
-import React from "react";
+import {
+  ArrowNarrowLeftIcon,
+  CheckIcon,
+  HomeIcon,
+  PaperClipIcon,
+  QuestionMarkCircleIcon,
+  SearchIcon,
+  ThumbUpIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
+
+const eventTypes = {
+  applied: { icon: UserIcon, bgColorClass: "bg-gray-400" },
+  advanced: { icon: ThumbUpIcon, bgColorClass: "bg-blue-500" },
+  completed: { icon: CheckIcon, bgColorClass: "bg-green-500" },
+};
+const timeline = [
+  {
+    id: 1,
+    type: eventTypes.applied,
+    content: "Applied to",
+    target: "Front End Developer",
+    date: "Sep 20",
+    datetime: "2020-09-20",
+  },
+  {
+    id: 2,
+    type: eventTypes.advanced,
+    content: "Advanced to phone screening by",
+    target: "Bethany Blake",
+    date: "Sep 22",
+    datetime: "2020-09-22",
+  },
+  {
+    id: 3,
+    type: eventTypes.completed,
+    content: "Completed phone screening with",
+    target: "Martha Gardner",
+    date: "Sep 28",
+    datetime: "2020-09-28",
+  },
+  {
+    id: 4,
+    type: eventTypes.advanced,
+    content: "Advanced to interview by",
+    target: "Bethany Blake",
+    date: "Sep 30",
+    datetime: "2020-09-30",
+  },
+  {
+    id: 5,
+    type: eventTypes.completed,
+    content: "Completed interview with",
+    target: "Katherine Snyder",
+    date: "Oct 4",
+    datetime: "2020-10-04",
+  },
+];
+const comments = [
+  {
+    id: 1,
+    name: "Leslie Alexander",
+    date: "4d ago",
+    imageId: "1494790108377-be9c29b29330",
+    body:
+      "Ducimus quas delectus ad maxime totam doloribus reiciendis ex. Tempore dolorem maiores. Similique voluptatibus tempore non ut.",
+  },
+  {
+    id: 2,
+    name: "Michael Foster",
+    date: "4d ago",
+    imageId: "1519244703995-f4e0f30006d5",
+    body:
+      "Et ut autem. Voluptatem eum dolores sint necessitatibus quos. Quis eum qui dolorem accusantium voluptas voluptatem ipsum. Quo facere iusto quia accusamus veniam id explicabo et aut.",
+  },
+  {
+    id: 3,
+    name: "Dries Vincent",
+    date: "4d ago",
+    imageId: "1506794778202-cad84cf45f1d",
+    body:
+      "Expedita consequatur sit ea voluptas quo ipsam recusandae. Ab sint et voluptatem repudiandae voluptatem et eveniet. Nihil quas consequatur autem. Perferendis rerum et.",
+  },
+];
 
 export default function UserProfile() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <main className="py-10">
+        {/* Page header */}
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
+          <div className="flex items-center space-x-5">
+            <div className="flex-shrink-0">
+              <div className="relative">
+                <img
+                  className="h-16 w-16 rounded-full"
+                  src="https://dota2.ru/img/heroes/pudge/portrait.jpg"
+                  alt=""
+                />
+                <span
+                  className="absolute inset-0 shadow-inner rounded-full"
+                  aria-hidden="true"
+                />
+              </div>
+            </div>
+            <div>
+              <h1 className="text-2xl font-bold text-gray-900">Anton Pudge</h1>
+              <p className="text-sm font-medium text-gray-500">
+                Status:{" "}
+                <a href="#" className="text-gray-900">
+                  Пользователь
+                </a>{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-8 max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
+          <div className="space-y-6 lg:col-start-1 lg:col-span-2">
+            {/* Description list*/}
+            <section aria-labelledby="applicant-information-title">
+              <div className="bg-white shadow sm:rounded-lg">
+                <div className="px-4 py-5 sm:px-6">
+                  <h2
+                    id="applicant-information-title"
+                    className="text-lg leading-6 font-medium text-gray-900"
+                  >
+                    Profile
+                  </h2>
+                </div>
+                <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+                  <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                    <div className="sm:col-span-1">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Email address
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900">
+                        Anton@anton.com
+                      </dd>
+                    </div>
+
+                    <div className="sm:col-span-1">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Likes
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900">120,000</dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+            </section>
+
+            {/* Answers*/}
+            <section aria-labelledby="notes-title">
+              <div className="bg-white shadow sm:rounded-lg sm:overflow-hidden">
+                <div className="divide-y divide-gray-200">
+                  <div className="px-4 py-5 sm:px-6">
+                    <h2
+                      id="notes-title"
+                      className="text-lg font-medium text-gray-900"
+                    >
+                      Answers
+                    </h2>
+                  </div>
+                  <div className="px-4 py-6 sm:px-6">
+                    <ul className="space-y-8">
+                      {comments.map((comment) => (
+                        <li key={comment.id}>
+                          <div className="flex space-x-3">
+                            <div className="flex-shrink-0"></div>
+                            <div>
+                              <div className="text-sm">
+                                <a
+                                  href="#"
+                                  className="font-medium text-gray-900"
+                                >
+                                  {comment.name}
+                                </a>
+                              </div>
+                              <div className="mt-1 text-sm text-gray-700">
+                                <p>{comment.body}</p>
+                              </div>
+                              <div className="mt-2 text-sm space-x-2">
+                                <span className="text-gray-500 font-medium">
+                                  {comment.date}
+                                </span>{" "}
+                                <span className="text-gray-500 font-medium">
+                                  &middot;
+                                </span>{" "}
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+
+          <section
+            aria-labelledby="timeline-title"
+            className="lg:col-start-3 lg:col-span-1"
+          >
+            <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:px-6">
+              <h2
+                id="timeline-title"
+                className="text-lg font-medium text-gray-900"
+              >
+                Questions
+              </h2>
+
+              {/* Questions */}
+              <div className="mt-6 flow-root">
+                <ul className="-mb-8">
+                  {timeline.map((item) => (
+                    <li key={item.id}>
+                      <div className="relative pb-8">
+                        <div className="relative flex space-x-3">
+                          <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                            <div>
+                              <p className="text-sm text-gray-500">
+                                {item.content}{" "}
+                                <a
+                                  href="#"
+                                  className="font-medium text-gray-900"
+                                >
+                                  {item.target}
+                                </a>
+                              </p>
+                            </div>
+                            <div className="text-right text-sm whitespace-nowrap text-gray-500">
+                              <time dateTime={item.datetime}>{item.date}</time>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-6 flex flex-col justify-stretch">
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Add Question
+                </button>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 }
