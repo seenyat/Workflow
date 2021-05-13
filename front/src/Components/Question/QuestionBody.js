efdcd57c81c1eec6bee5a69b93493f7062f7e5d3
Roman Zagrebin <seenyat@gmail.com>
2021-05-13 12:20:00 +0300

    answers working

--- front/src/Components/Question/QuestionBody.js
+++ front/src/Components/Question/QuestionBody.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-export default function QuestionBody({ question }) {
+export default function QuestionBody({ questions }) {
   return (
     <div className="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
       <div className="px-4 py-5 sm:px-6">
-        <p className="font-bold text-3xl">{question.title}</p>
-        <p>{question.body}</p>
+        <p className="font-bold text-3xl">{questions.title}</p>
+        <p>{questions.body}</p>
       </div>
       <div className="px-4 py-5 text-gray-600 text-sm sm:p-6">
-        {question.author ?? "Антон"}
+        {questions.author ?? "Антон"}
       </div>
     </div>
   );
--- front/src/Views/QuestionSingle.js
+++ front/src/Views/QuestionSingle.js
@@ -3,12 +3,15 @@ import { useParams } from "react-router";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import QuestionBody from "../Components/Question/QuestionBody";
+import CreateAnswer from "../Components/Question/CreateAnswer";
+import AnswerList from "../Components/Question/AnswerList";
 
 export default function QuestionSingle() {
   const { id } = useParams();
 
   const [editStatus, setEditStatus] = useState(false);
-  const [question, setQuestion] = useState({});
+  const [editCount, setEditCount] = useState(0);
+  const [questionObj, setQuestion] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:4000/question/${id}`).then((data) =>
@@ -17,44 +20,13 @@ export default function QuestionSingle() {
         setQuestion(question);
       })
     );
-  }, [id]);
+  }, [id, editCount]);
 
-  async function addAnswer(e) {
-    e.preventDefault();
-    fetch("http://localhost:4000/answer", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        workflows: e.target.todo.value,
-        comment: e.target.title.value,
-        id: id,
-      }),
-    });
-  }
-
-  return question ? (
-    <div className="container py-2 mx-auto px-4 sm:px-6 lg:px-8">
-      <QuestionBody question={question} />
-      {!editStatus && (
-        <button
-          onClick={() => setEditStatus(true)}
-          className="flex justify-center items-center p-3 h-24 w-24 border border-transparent rounded-full text-6xl shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          +
-        </button>
-      )}
-
-      <form
-        onSubmit={addAnswer}
-        className="flex flex-col space-y-3 m-5"
-        action=""
-      >
-        <input type="text" placeholder="title" name="title" />
-        <input type="text" placeholder="todo" name="todo" />
-        <button>Submit </button>
-      </form>
+  return questionObj ? (
+    <div className="overflow-scroll container py-2 mx-auto px-4 sm:px-6 lg:px-8">
+      <QuestionBody questions={questionObj.question} />
+      <CreateAnswer count={editCount} edit={setEditCount} id={id} />
+      <AnswerList answers={questionObj.answers} />
     </div>
   ) : null;
 }
