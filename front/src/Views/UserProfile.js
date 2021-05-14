d733149eed4ab9767aac13e9493e57795577fc2d
Andrey <trifonova86@mail.ru>
2021-05-14 10:12:32 +0300

    morning

--- front/src/Views/UserProfile.js
+++ front/src/Views/UserProfile.js
@@ -58,41 +58,20 @@ const timeline = [
     datetime: "2020-10-04",
   },
 ];
-const comments = [
-  {
-    id: 1,
-    name: "Leslie Alexander",
-    date: "4d ago",
-    imageId: "1494790108377-be9c29b29330",
-    body: "Ducimus quas delectus ad maxime totam doloribus reiciendis ex. Tempore dolorem maiores. Similique voluptatibus tempore non ut.",
-  },
-  {
-    id: 2,
-    name: "Michael Foster",
-    date: "4d ago",
-    imageId: "1519244703995-f4e0f30006d5",
-    body: "Et ut autem. Voluptatem eum dolores sint necessitatibus quos. Quis eum qui dolorem accusantium voluptas voluptatem ipsum. Quo facere iusto quia accusamus veniam id explicabo et aut.",
-  },
-  {
-    id: 3,
-    name: "Dries Vincent",
-    date: "4d ago",
-    imageId: "1506794778202-cad84cf45f1d",
-    body: "Expedita consequatur sit ea voluptas quo ipsam recusandae. Ab sint et voluptatem repudiandae voluptatem et eveniet. Nihil quas consequatur autem. Perferendis rerum et.",
-  },
-];
+
 
 export default function UserProfile() {
   const user = useSelector((state) => state.user);
+  console.log(user);
   const [answer, setAnswer] = useState();
   useEffect(() => {
-    fetch(`http://localhost:4000/profile/609d3bd25b04e824c45be357`, {
+    fetch(`http://localhost:4000/profile/${user._id}`, {
       method: "GET",
       credentials: "include",
     }).then((data) =>
       data.json().then((profile) => {
         // setAnswer(profile)
-
+console.log(profile)
         setAnswer(profile);
       })
     );
