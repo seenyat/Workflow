6dda5858da6119ddea1afe79ecb210b0e4f221a9
Roman Zagrebin <seenyat@gmail.com>
2021-05-18 15:59:12 +0300

    verstka

--- front/src/Components/Question/CommentContent.js
+++ front/src/Components/Question/CommentContent.js
@@ -1,13 +1,19 @@
 import Time from "../../Utils/Time";
 import AuthorCard from "../Partials/AuthorCard";
+import Like from "../Partials/Like";
 
 function CommentContent({ content }) {
   return (
-    <div className="mt-2">
-      <AuthorCard author={content.author} />
+    <div className="mb-4 bg-gray-50 relative w-max max-w-full px-3 py-2 rounded">
+      <div className="mr-16">
+        <AuthorCard author={content.author} />
+      </div>
       <div className="py-1">{content.content}</div>
       <div className="text-xs text-gray-400">
         <Time time={content.date} />
+        <div className="absolute text-xs top-1 right-1">
+          <Like />
+        </div>
       </div>
     </div>
   );
--- front/src/Components/Question/CommentForm.js
+++ front/src/Components/Question/CommentForm.js
@@ -1,3 +1,4 @@
+import { XIcon } from "@heroicons/react/outline";
 import { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { sagaAddCommentAC } from "../../Redux/actions/actionCreator";
@@ -22,9 +23,29 @@ function CommentForm({ answer, statusTextArea }) {
   };
 
   return (
-    <form className="flex flex-col py-3 px-5 space-y-3 w-full rounded max-w-3xl shadow my-5" onSubmit={addComment}>
-      <textarea className="rounded text-sm" ref={commentText} placeholder="Ваш комментарий..."></textarea>
-      <button className="rounded bg-indigo-400 hower:bg-indigo-600 text-white py-2 w-max px-3" type="submit">Отправить</button>
+    <form
+      className="flex flex-col py-3  px-5 space-y-3 relative w-full rounded max-w-3xl shadow my-5"
+      onSubmit={addComment}
+    >
+      <div className="font-bold">
+        <XIcon
+          onClick={() => statusTextArea.setTextArea(false)}
+          className="w-8 h-8 text-gray-300 hover:text-black cursor-pointer transition absolute right-2 top-2"
+        />
+        Добавить комментарий
+      </div>
+      <textarea
+        className="rounded-md sm:mr-32 h-24 placeholder-gray-300 border-gray-200 resize-none text-sm"
+        ref={commentText}
+        placeholder="Ваш комментарий..."
+      ></textarea>
+
+      <button
+        className="rounded hover:bg-indigo-700 transition bg-indigo-400 hower:bg-indigo-600 text-white py-2 w-max px-3"
+        type="submit"
+      >
+        Отправить
+      </button>
     </form>
   );
 }
