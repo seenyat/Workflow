7d34224de31dc8ee2c6de4ff20de39f185414f7f
Anton <antonmanafov@Mail.ru>
2021-05-20 09:52:10 +0300

    cards

--- front/src/Components/Member.js
+++ front/src/Components/Member.js
@@ -1,14 +1,15 @@
 export default function Member({ author }) {
-  console.log(author);
   return author ? (
-    <div className="h-full w-full flex flex-row items-center">
-      <div className=" h-max min-w-max flex justify-center items-center ">
-        <img src={`${author.img}`} alt="" />
-      </div>
-      <div className="bg-gray-200 p-16 h-full w-full font-extrabold text-3xl text-black items-center flex flex-col">
-        <span className="text-8xl mb-14 ">{author.name}</span>
-        <span> {author.position} </span>
-        <span>{author.status}</span>
+    <div className="h-full w-full flex flex-col ">
+      <div className="h-full w-full flex flex-row items-center z-0 ">
+        <div className=" h-max min-w-max flex justify-center items-center ">
+          <img src={`${author.img}`} alt="" />
+        </div>
+        <div className="bg-gray-200 p-16 h-full w-full font-extrabold text-3xl text-black items-center flex flex-col">
+          <span className="text-8xl mb-14 ">{author.name}</span>
+          <span> {author.position} </span>
+          <span>{author.status}</span>
+        </div>
       </div>
     </div>
   ) : null;
---
+++ front/src/Views/Stack.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Cardslist from "../Utils/Cardslist";
+
+export default function Stack() {
+  return (
+    <div className="flex w-full h-full relative flex-col">
+      <Link to="/about">
+        <div className="w-full h-20  absolute z-20 text-3xl flex justify-center items-center outline-none">
+          Перейти к команде
+        </div>
+      </Link>
+      <Cardslist />
+    </div>
+  );
+}
